9c091c5deb067c7b80d7bf1ca138d657129d7684
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2021-11-13 15:37:39 -0800

    Make SclerodeoMenu_main the standard page frame main menu

--- classes/SclerodeoMenu_main/SclerodeoMenu_main.php
+++ classes/SclerodeoMenu_main/SclerodeoMenu_main.php
@@ -1,11 +1,14 @@
 <?php
 
-final class SclerodeoMenu_main {
+final class
+SclerodeoMenu_main {
 
     /**
      * @return void
      */
-    static function CBInstall_install(): void {
+    static function
+    CBInstall_install(
+    ): void {
         $updater = CBModelUpdater::fetch(
             (object)[
                 'className' => 'CBMenu',
@@ -15,23 +18,42 @@ final class SclerodeoMenu_main {
             ]
         );
 
-        CBModelUpdater::save($updater);
+        CBModelUpdater::save(
+            $updater
+        );
+
+        CB_StandardPageFrame::setDefaultMainMenuModelCBID(
+            SclerodeoMenu_main::ID()
+        );
     }
+    /* CBInstall_install() */
+
+
 
     /**
      * @return [string]
      */
-    static function CBInstall_requiredClassNames(): array {
+    static function
+    CBInstall_requiredClassNames(
+    ): array {
         return [
+            'CB_StandardPageFrame',
             'CBMenu',
             'CBModelUpdater',
         ];
     }
+    /* CBInstall_requiredClassNames() */
+
+
 
     /**
      * @return ID
      */
-    static function ID(): string {
+    static function
+    ID(
+    ): string {
         return '495535e5ac48b2b68a83aea9b2789e8bda968807';
     }
+    /* ID() */
+
 }
